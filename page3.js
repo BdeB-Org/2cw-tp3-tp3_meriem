27bb8b901360a1c1834c045d113b37e3e10d473f
Meriem Taha <22426982@bdeb.qc.ca>
2024-05-30 12:46:38 -0400

    modification

--- page3.js
+++ page3.js
@@ -1,10 +1,12 @@
 function animalRecherche() {
-    const animal = document.getElementById("animalInput").value;
+    const animal = document.getElementById("userResponse").value;
     const resultat = document.getElementById("resultatAnimal");
     resultat.innerHTML = `<h2>Resultats pour : ${animal}</h2><p> Informations sur l'animal $ {animal}...</p>`;
 
     const animalForm = document.getElementById('animalForm');
     if (animalForm) {
         animalForm.addEventListener('submit', searchAnimal);
+        searchDatabase('animals', 'userResponse', 'resultatAnimal')
+
     }
 }
