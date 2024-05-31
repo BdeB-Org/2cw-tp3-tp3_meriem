d141b8f3a2faa0241d53047b60d37cb7eeb5c8b1
Meriem Taha <22426982@bdeb.qc.ca>
2024-05-30 22:39:35 -0400

    modification

--- page4.js
+++ page4.js
@@ -3,17 +3,38 @@ function submit() {
     searchInput = document.getIdentifiedBy("search-input").value;
     bookResults = document.getIdentifiedBy("book-results").value;
 
-    const url = 'https://vscode.dev/github/BdeB-Org/2cw-tp3-tp3_meriem/blob/main';
+    function fetchData(title, author, description) {
+        $.ajax({
+            method: 'GET',
+            url: 'https://github.com/BdeB-Org/2cw-tp3-tp3_meriem/blob/main/data.sql',
+            contentType: 'application/sql',
+            success: function (result) {
+                console.log(result);
+
+                result(books);
+            },
+            error: function ajaxError(jqXHR) {
+                console.error('Error: ', jqXHR.responseText);
+            }
+        });
+    }
     fetch(url)
-        .then((resp) => resp.json())
+        .then((resp) => resp.sql())
         .then(function (data) {
             let books = data.items;
-            return books.map(function (book) {
+            return books.map(function (books) {
                 let li = createNode("li"),
                     span = createNode("span");
                 span.innerHTML = `${books.title} ${books.author} ${books.description}`;
                 append(li, span);
                 append(book_ul, li);
             });
+        })
+        .catch(function (error) {
+            console.log(JSON.stringify(error));
         });
+
 }
+
+
+
